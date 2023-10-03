83c507612ca983646ce406bf2403eb3ca1073e92
smith <smithbabawande@gmail.com>
2023-10-03 09:53:06 +0100

    Completed Assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    badge = (f"Hello, my name is {name}.")
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    badges = [f"Hello, my name is {name}." for name in names]
+    # Remember list comprehensions return lists,(now to get the for loop method working:)
+    return badges
+
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i, name in enumerate(names, start=1):
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {i}!")
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    for assignment in room_assignments:
+        print(assignment)
